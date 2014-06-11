eb941155eb029ae9117931302f2c2de0b54b690d
Xkonti <online.xkonti@gmail.com>
2014-06-11 16:14:21 +0200

    Dodanie Zwiedzanej i Wypoczynkowej

---
+++ src/biuropodrozy/WycieczkaWypoczynkowa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package biuropodrozy;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author onlin_000
+ */
+public class WycieczkaWypoczynkowa extends Wycieczka implements Serializable {
+    
+    private String udogodnienia;
+    
+    public String getUdogodnienia() { return udogodnienia; }
+    public void setUdogodnienia(String newUdogodnienia) { udogodnienia = newUdogodnienia; }
+    
+}
