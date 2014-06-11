6d598846d587cf18ebd17782c472a15d2158220c
Rossi1994 <roszak.paulina1994@gmail.com>
2014-06-11 15:53:03 +0200

    start projektu

---
+++ src/biuropodrozy/Wycieczka.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package biuropodrozy;
+
+/**
+ *
+ * @author Paulina
+ */
+public class Wycieczka {
+    
+    private String opis;
+    private int cena;
+    private String kategoria;
+    private String transport;
+    private String wyzywienie;
+    private String wyjazdz;
+    private String data;
+    private int iloscMiejsc;
+    private int dlugosc;
+    
+    public String getOpis() { return opis; }
+    public int getCena() { return cena; }
+    public String getKategoria() { return kategoria; }
+    public String getTransport() { return transport; }
+    public String getWyzywienie() { return wyzywienie; }
+    public String getWyjazdz() { return wyjazdz; }
+    public String getData() { return data; }
+    public int getiloscMiejsc() { return iloscMiejsc; }
+    public int getDlugosc() { return dlugosc; }
+}
